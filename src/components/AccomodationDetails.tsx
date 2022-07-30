5ad30b08ebe115ec299c3f23c036af01d4618827
Amar K <amar.karamehmedovic7@gmail.com>
2022-07-30 21:01:19 +0200

    Editing details

--- src/components/AccomodationDetails.tsx
+++ src/components/AccomodationDetails.tsx
@@ -1,8 +1,92 @@
+import { Rating } from '@mui/material';
 import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import ImageIcon from '@mui/icons-material/Image';
+import './AccomodationDetails.css';
+import {
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBBtn
+} from 'mdb-react-ui-kit';
 
-function AccomodationDetails() {
+type AccomodationDetailsProps = {
+    title: string;
+    subtitle: string;
+    description: string;
+    type: string;
+    categorization: number;
+    personCount: number;
+    imageUrl: string;
+    freeCancelation: boolean;
+    price: number;
+    location: string;
+    postalCode: string;
+};
+
+const AccomodationDetails = (props: AccomodationDetailsProps) => {
+    const title = props.title;
+    const subtitle = props.subtitle;
+    const description = props.description;
+    const type = props.type;
+    const categorization = props.categorization;
+    const personCount = props.personCount;
+    const imageUrl = props.imageUrl;
+    const freeCancelation = props.freeCancelation;
+    const price = props.price;
+    const location = props.location;
+    const postalCode = props.postalCode;
+
     return (
-        <div>AccomodationDetails</div>
+        <Container fluid>
+            <Row>
+                <Col><div className="hero-image" /></Col>
+            </Row>
+            <Row>
+                <Col sm={8}>
+                    <Card.Body>
+                        <Card.Title className='acc-details-title'>
+                            {title}
+                            &nbsp;&nbsp;&nbsp;
+                            <Rating className='acc-details-rating' name="read-only" value={categorization} readOnly />
+                        </Card.Title>
+                        <Card.Text className='acc-details-subtitle'>{subtitle}</Card.Text>
+                        <br></br>
+                        <Card.Text className='acc-details-cancelation'>
+                            <ImageIcon fontSize='medium' sx={{ color: '#F3B08E' }} />
+                            &nbsp;&nbsp;&nbsp;
+                            <b>{freeCancelation ? 'Free cancellation available' : 'Free cancellation is not available'}</b>
+                        </Card.Text>
+                        <br></br>
+                        <Card.Text className='acc-details-description'>
+                            {description}
+                        </Card.Text>
+                    </Card.Body>
+                </Col>
+                <Col sm={1}>
+                    empty
+                </Col>
+                <Col sm={3} className='acc-details-info-card-wrapper'>
+                    <br></br>
+                    <MDBCard className='acc-details-info-card'>
+                        <MDBCardBody className='acc-details-info-card-body'>
+                            <MDBCardTitle className='info-card-title'>Property info</MDBCardTitle>
+                            <br></br>
+                            <MDBCardText className='info-card-text'>{personCount}</MDBCardText>
+                            <MDBCardText className='info-card-text'>{type}</MDBCardText>
+                            <MDBCardText className='info-card-text'>EUR {price} per night</MDBCardText>
+                            <MDBCardText className='info-card-text'>{location}</MDBCardText>
+                            <MDBCardText className='info-card-text'>{postalCode}</MDBCardText>
+                            <br></br>
+                            <div className='info-card-btn-wrapper'>
+                                <MDBBtn className='info-card-btn'>BOOK A STAY &gt;</MDBBtn>
+                            </div>
+                        </MDBCardBody>
+                    </MDBCard>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
